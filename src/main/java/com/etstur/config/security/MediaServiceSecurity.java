608c7db1fe4d4b873540ca713ef5ca83f8ecf17d
aydinmertcan <ayd.mertcan@gmail.com>
2022-03-17 01:51:00 +0300

    fix: Authorization error in the static files is fixed.

--- src/main/java/com/etstur/config/security/MediaServiceSecurity.java
+++ src/main/java/com/etstur/config/security/MediaServiceSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -19,11 +20,14 @@ public class MediaServiceSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.headers().frameOptions().disable();
         http.authorizeRequests().antMatchers("/v3/api-docs/**",
                         "/swagger-ui/**",
                         "/user/token",
-                        "/media/save").permitAll()
+                        "/media/save",
+                        "/index.html/**", "/static/**", "/").permitAll()
                 .anyRequest().authenticated();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(getJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
     }
